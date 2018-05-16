bdf9ca5cf920ca59f8305be6c893a08c49e6c28c
Bohdan Ivanov <bogdanivanov@live.com>
2018-05-16 22:07:28 +0300

    Added Colorama to setup.py required packages

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
 
     include_package_data=True,
 
-    install_requires=['pbxproj', 'jinja2', 'pyyaml'],
+    install_requires=['pbxproj', 'jinja2', 'pyyaml', 'colorama'],
 
     entry_points={
         'console_scripts': [
