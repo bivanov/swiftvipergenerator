76a28d22ceeacc885013cf9a2a0dd01e724ea484
Bohdan Ivanov <bogdanivanov@live.com>
2018-05-21 11:38:32 +0300

    Interactor delegate protocol; Presenter conformance to this protocol

--- swivigen/Templates/Interactor.tpl.swift
+++ swivigen/Templates/Interactor.tpl.swift
@@ -1,6 +1,10 @@
 {% extends "Header.tpl.swift" %}
 {% block content %}
-class {{ module_name  }}Interactor {
+protocol {{ module_name }}InteractorDelegate: class {
     
+}
+
+class {{ module_name  }}Interactor {
+    public weak var delegate: {{ module_name }}InteractorDelegate?    
 }
 {% endblock %}
--- swivigen/Templates/Presenter.tpl.swift
+++ swivigen/Templates/Presenter.tpl.swift
@@ -1,7 +1,11 @@
 {% extends "Header.tpl.swift" %}
 {% block content %}
-class {{ module_name }}Presenter: AbstractPresenter {
-    var interactor: {{ module_name }}Interactor!
+class {{ module_name }}Presenter: AbstractPresenter, {{ module_name }}InteractorDelegate {
+    var interactor: {{ module_name }}Interactor! {
+        didSet {
+            self.interactor.delegate = self
+        }
+    }
     var router: {{ module_name }}Router!
     weak var view: {{ module_name }}View!
 }
